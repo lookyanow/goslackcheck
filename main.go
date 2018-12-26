7f150c7055faa60ec14b7519deeca07ac758fb0e
Lukyanov Ivan <ilukyanov@ozon.ru>
2018-12-26 21:00:31 +0300

    slack userfind by email added

--- main.go
+++ main.go
@@ -17,8 +17,7 @@ func main() {
 
 	api := slack.New(strings.TrimSpace(string(b)))
 	attachment := slack.Attachment{
-		Pretext: "some pretext",
-		Text:    "test message",
+		Text:    "test message <https://gitlab.ozon.ru/bx/layout-api/-/jobs/810408|helm-check>",
 		// Uncomment the following part to send a field too
 		/*
 			Fields: []slack.AttachmentField{
@@ -30,11 +29,24 @@ func main() {
 		*/
 	}
 
-	channelID, timestamp, err := api.PostMessage("gitlab", slack.MsgOptionText("Some text", false), slack.MsgOptionAttachments(attachment))
+	channelID, timestamp, err := api.PostMessage("gitlab", slack.MsgOptionText("Test message", false), slack.MsgOptionAttachments(attachment))
 	if err != nil {
 		fmt.Printf("%s\n", err)
 		return
 	}
-	fmt.Printf("Message successfully sent to channel %s at %s", channelID, timestamp)
+	fmt.Printf("Message successfully sent to channel %s at %s\n", channelID, timestamp)
+
+	user, err := api.GetUserByEmail("ilukyanov@ozon.ru")
+	if err != nil{
+		fmt.Println(err)
+	}
+	fmt.Printf("User:%s Full name:%s\n", user.Name, user.RealName)
+	userName := "@" + user.Name
+	userID, _, err1 := api.PostMessage(userName, slack.MsgOptionText("Test message", false), slack.MsgOptionAttachments(attachment), slack.MsgOptionUsername("ilukyanov"))
+	if err1 != nil {
+		fmt.Println(err1)
+	}
+	fmt.Printf("Message successfully sent to user %s\n", userID )
+
 
 }
